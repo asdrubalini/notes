8e6996eb94f60a494fc38228dce5ab9fe12c9a5b
asdrubalini <asdrubalini@mail.com>
2022-04-29 21:20:14 +0200

    does this work

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -46,14 +46,18 @@
   :straight t
   :ensure t)
 
-("org-notes"
-  :base-directory "./"
-  :base-extension "org"
-  :publishing-directory "./publish"
-  :recursive t
-  :publishing-function org-html-publish-to-html
-  :headline-levels 4
-  :auto-preamble t
- )
+(setq org-publish-project-alist
+    '(
+	("org-notes"
+	    :base-directory "./"
+	    :base-extension "org"
+	    :publishing-directory "./publish"
+	    :recursive t
+	    :publishing-function org-html-publish-to-html
+	    :headline-levels 4
+	    :auto-preamble t
+	)
 
-("org" :components ("org-notes"))
+	("org" :components ("org-notes"))
+    ))
+
